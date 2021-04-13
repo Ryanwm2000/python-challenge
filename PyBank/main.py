3d354e2c7a6626d054a3ceed3fff7311ab68eaca
Ryanwm2000 <80086540+Ryanwm2000@users.noreply.github.com>
2021-04-13 14:51:47 -0700

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,7 +2,7 @@ import os
 import csv
 
 numonths = 0
-totalchange = 0
+tc = 0
 maxinc = 0
 maxincdate = ""
 maxdec = 0
@@ -34,14 +34,14 @@ with open(csvpath, newline='') as csvfile:
 
         previous = current
         numonths += 1
-        totalchange += float(row[1])
+        tc += float(row[1])
 
 avgchange = avgchange / (numonths-1)
 
 results = []
 results.append("Financial Analysis\n----------------------------")
 results.append(f"Total Months: {numonths}")
-results.append(f"Total: ${round(totalchange)}")
+results.append(f"Total: ${round(tc)}")
 results.append(f"Average Change: ${round(avgchange,2)}")
 results.append(f"Greatest Increase in Profits: {maxincdate} (${round(maxinc)})")
 results.append(f"Greatest Decrease in Profits: {maxdecdate} (${round(maxdec)})")
