17869274a8b2fb480bb54a7ec1d446e0d91ea05a
Ryanwm2000 <80086540+Ryanwm2000@users.noreply.github.com>
2021-04-12 22:35:57 -0700

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@ import csv
 
 total_votes = 0
 candidates = []
-votesPerCandidates = []
+votesPerCandidate = []
 
 csvpath = os.path.join('election_data.csv')
 with open(csvpath, newline='') as csvfile:
@@ -14,27 +14,27 @@ with open(csvpath, newline='') as csvfile:
         total_votes += 1
         if total_votes == 1:
             candidates.append(row[2])
-            votesPerCandidates.append(1)
+            votesPerCandidate.append(1)
         else:
             try:
                 ican = candidates.index(row[2])
-                votesPerCandidates[ican] += 1
+                votesPerCandidate[ican] += 1
             except:
                 candidates.append(row[2])
-                votesPerCandidates.append(1)
+                votesPerCandidate.append(1)
 
 results = []
 results.append("Election Results\n-------------------------")
 results.append(f"Total Votes: {total_votes}\n-------------------------")
 
 winner = candidates[0]
-maxvotes = votesPerCandidates[0]
+maxvotes = votesPerCandidate[0]
 for i in range(len(candidates)):
-    if votesPerCandidates[i] > maxvotes:
+    if votesPerCandidate[i] > maxvotes:
         winner = candidates[i]
-        maxvotes = votesPerCandidates[i]
-    percent = 100 * votesPerCandidates[i] / total_votes
-    results.append(f"{candidates[i]}: {round(percent,3)} % ({votesPerCandidates[i]})")
+        maxvotes = votesPerCandidate[i]
+    percent = 100 * votesPerCandidate[i] / total_votes
+    results.append(f"{candidates[i]}: {round(percent,3)} % ({votesPerCandidate[i]})")
 
 results.append(f"-------------------------\nWinner: {winner}\n-------------------------")
 
